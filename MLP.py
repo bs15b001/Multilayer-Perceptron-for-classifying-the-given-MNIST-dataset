62e11cddd245314d45c261d205a23c5ad28a3482
gouraakash <gouraakash@gouraakashs-MacBook-Air.local>
2020-05-24 16:29:49 +0530

    train loss and validation loss plot is added

--- MLP.py
+++ MLP.py
@@ -7,9 +7,17 @@ import numpy as np
 import time 
 import pickle
 
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from keras.layers import Activation, Dense
+
 
 # Global parameters
 num_classes=10
+input_layer=28*28
+batch=50
+epoch=100
 
 
 # the data, shuffled and split between a train and test sets 
@@ -45,9 +53,43 @@ Y_test=np_utils.to_categorical(y_test,num_classes)
 
 print("After converting one hot vector:::: ", Y_train[0])
 
-def plot_functions(x, vy, ty, ax ,color=['b']):
-    ax.plot(x,vy,'b',label='Validation loss')
-    ax.plot(x,ty,'r',label='Train loss')
-    plt.legend()
-    plt.grid()
-    fig.canvas.draw()
+# The Sequential model is a linear stack of layers. we can create a Sequential model by passing a list of layer instances to the constructor
+# Reference : https://keras.io/guides/sequential_model/
+# start building model 
+
+model = keras.Sequential()
+model.add(layers.Dense(num_classes,input_dim=input_layer,activation='softmax'))
+
+# define An optimizer , A loss function, A metrics
+# sgd=Stochastic gradient descent
+model.compile(optimizer='sgd', loss='categorical_crossentropy',metrics=['accuracy'])
+
+
+# Train 
+history = model.fit(X_train,Y_train,batch_size=batch,epochs=epoch,verbose=1,validation_data=(X_test,Y_test))
+
+
+print("Model summary::: ",model.summary())
+
+# Evaluation
+score=model.evaluate(X_test,Y_test,verbose=0)
+print('Test score: ',score[0])
+print('Test accuracy ',score[1])
+
+fig,ax =plt.subplots(1,1)
+ax.set_xlabel('epoch')
+ax.set_ylabel('categorical crossentropy loss')
+
+x =  list(range(1,epoch+1))
+
+vy=history.history['val_loss']
+ty=history.history['loss']
+ax.plot(x,vy,'b',label='Validation loss')
+ax.plot(x,ty,'r',label='Train loss')
+plt.legend()
+plt.grid()
+plt.show()
+
+
+
+
